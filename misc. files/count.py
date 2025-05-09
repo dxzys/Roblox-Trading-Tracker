a12fccb4a1cbfd7a7cea2442c3936ef442563d33
dxzys <76858431+dxzys@users.noreply.github.com>
2025-05-09 10:49:53 -0300

    Update count.py

--- misc. files/count.py
+++ misc. files/count.py
@@ -1,11 +1,11 @@
 # This was used to count the number of UAIDs I had logged for a particular limited ID, if any records existed in the uaidLog folder.
 # Only works properly if you move this to the main project folder and run it from there.
 import os
-import json
+from pathlib import Path
 
 def count_uaids(log_folder, limited_id):
-    json_file_path = os.path.join(log_folder, f"{limited_id}.json")
-    if not os.path.exists(json_file_path):
+    json_file_path = log_folder / f"{limited_id}.json"
+    if not json_file_path.exists():
         print(f"No log file found for limited: {limited_id}")
         return
     with open(json_file_path, 'r') as file:
@@ -17,8 +17,8 @@ def count_uaids(log_folder, limited_id):
 
 def main():
     limited_id = input("Enter the limited ID: ")
-    main_project_folder = os.path.dirname(os.path.abspath(__file__))
-    log_folder = os.path.join(main_project_folder, 'uaidLog')
+    main_project_folder = Path(__file__).parent.absolute()
+    log_folder = main_project_folder / 'uaidLog'
     count_uaids(log_folder, limited_id)
 
 if __name__ == "__main__":
